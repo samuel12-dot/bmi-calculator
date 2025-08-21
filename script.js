b8e72ffa7c97339c7194e1951cd93f1a1e4443dd
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-21 01:49:09 +0100

    fea: add function to calculate and reset bmi

---
+++ script.js
@@ -0,0 +1,29 @@
+//function
+//get values
+//calculate
+//output
+
+//formula for BMI - weight/height * 703
+
+function calculateBMI() {
+    let weight = document.getElementById('weight').value
+    let height = document.getElementById('height').value
+
+    let bmi = (weight / (height * height) * 703) 
+
+    document.getElementById('heading').innerHTML = 'Your BMI is: '
+    document.getElementById('bmi-output').innerHTML = bmi.toFixed(2)
+
+    if( bmi <= 24.9) {
+        document.getElementById('message').innerHTML = 'You are underweight'
+    }else if( bmi >= 25 && bmi <= 29.9) {
+        document.getElementById('message').innerHTML = 'You have a healthy weight'
+    }else {
+        document.getElementById('message').innerHTML = 'You are overweight'
+    }
+}
+
+
+function resetBMI() {
+    window.location.reload()
+}
