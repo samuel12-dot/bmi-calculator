5a2ae90fc7b383cab3bcf20b0ecf406216533f08
samuel12-dot <samuelogunduyile76@gmail.com>
2025-08-21 02:48:54 +0100

    feat: finalise bmi calculator

--- script.js
+++ script.js
@@ -6,20 +6,26 @@
 //formula for BMI - weight/height * 703
 
 function calculateBMI() {
-    let weight = document.getElementById('weight').value
-    let height = document.getElementById('height').value
+    if (weight.value == '' || height.value == '') {
+        alert(`You must enter the value of height and weight`)
+    } else {
+        let weight = document.getElementById('weight').value
+        let height = document.getElementById('height').value
 
-    let bmi = (weight / (height * height) * 703) 
+        let bmi = (weight / ((height * height)/100))
 
-    document.getElementById('heading').innerHTML = 'Your BMI is: '
-    document.getElementById('bmi-output').innerHTML = bmi.toFixed(2)
+        document.getElementById('heading').innerHTML = 'Your BMI is: '
+        document.getElementById('bmi-output').innerHTML = bmi.toFixed(2)
 
-    if( bmi <= 24.9) {
-        document.getElementById('message').innerHTML = 'You are underweight'
-    }else if( bmi >= 25 && bmi <= 29.9) {
-        document.getElementById('message').innerHTML = 'You have a healthy weight'
-    }else {
-        document.getElementById('message').innerHTML = 'You are overweight'
+        if (bmi <= 18.5) {
+            document.getElementById('message').innerHTML = 'You are underweight. Try eating more nutrient-rich foods.'
+        } else if (bmi >= 18.5 && bmi <= 24.9) {
+            document.getElementById('message').innerHTML = 'Your weight is healthy. Keep up your good habits.'
+        } else if(bmi >= 25 && bmi <= 29.9){
+            document.getElementById('message').innerHTML = 'You are overweight. Exercise more and watch your diet.'
+        }else {
+            document.getElementById('message').innerHTML = 'You are obese. Seek medical advice for weight management.'
+        }
     }
 }
 
